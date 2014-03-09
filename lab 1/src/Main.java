98dd43fed6ffe442ab869d260bb331f1bf4da043
4dam <amazush@gmail.com>
2014-03-09 12:11:58 +0100

    as

--- lab 1/src/Main.java
+++ lab 1/src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-	int max;
+	int max = 100;
 	Student[] stud = new Student[max];
 
 	public static void main(String[] args) {
@@ -9,27 +9,30 @@ public class Main {
 	}
 
 	public void wypiszWTabeli() {
-		System.out.printf("|%-19s|%-19s|%10d|%3d|%7.2f|\n", "Nazwisko", "Imiê", "Album", "Rok", "Œrednia" );
+		System.out.printf("|%-19s|%-19s|%10d|%3d|%7.2f|\n", "Nazwisko", "Imiê",
+				"Album", "Rok", "Œrednia");
 		for (int i = 0; i < max; i++) {
 			System.out.println(stud[i]);
 		}
 	}
-	
+
 	public void wczytajZWejscia() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Podaj kolenjo: nazwisko, imiê, album, rok i œredni¹.");
-		
-		if(stud.length < max) {
-			String nazwisko = sc.next();
-			String imie = sc.next();
-			long album = sc.nextLong();
-			int rok = sc.nextInt();
-			double srednia = sc.nextDouble();
-			//JAK TU DODAC STUDENTA DO TABLICY?!
-					
-		}
-		else { System.out.println("Za du¿o studentów.");
-	}
+		System.out
+				.println("Podaj kolenjo: nazwisko, imiê, album, rok i œredni¹.");
 
-}
+		for (int i = 0; i < max; i++) {
+			if (stud[i] == null) {
+				String nazwisko = sc.next();
+				String imie = sc.next();
+				long album = sc.nextLong();
+				int rok = sc.nextInt();
+				double srednia = sc.nextDouble();
+
+				stud[i] = new Student(nazwisko, imie, album, rok, srednia);
+			}
+
+		}
+
+	}
 }
